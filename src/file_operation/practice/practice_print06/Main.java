9be0691d1ad2fe171a967289fc706c7d969ef022
yuicya06 <122197122+yuicya06@users.noreply.github.com>
2024-10-23 08:17:08 +0900

    fetch merge

--- src/file_operation/practice/practice_print06/Main.java
+++ src/file_operation/practice/practice_print06/Main.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
-
+//fetch test merge test
 public class Main {
 	public static void main(String[] args) throws ParseException {
 
